6adf5dcf6034f1b16900eee8ab2edbb83337c7e5
Alexander Werning <awerning@mail.upb.de>
2019-08-20 13:47:50 +0200

    Fixed merging of labels with data instances object

--- JAICore/jaicore-ml/src/main/java/ai/libs/jaicore/ml/evaluation/evaluators/weka/ClusteringValidationEvaluator.java
+++ JAICore/jaicore-ml/src/main/java/ai/libs/jaicore/ml/evaluation/evaluators/weka/ClusteringValidationEvaluator.java
@@ -6,14 +6,13 @@ import ai.libs.jaicore.basic.algorithm.exceptions.ObjectEvaluationFailedExceptio
 import ai.libs.jaicore.ml.core.evaluation.measure.IMeasure;
 import ai.libs.jaicore.ml.core.evaluation.measure.unlabeled.IInternalClusteringValidationMeasure;
 import ai.libs.jaicore.ml.evaluation.IInstancesClassifier;
-import java.util.ArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import weka.classifiers.Classifier;
-import weka.core.Attribute;
-import weka.core.DenseInstance;
-import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.SelectedTag;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Add;
 
 public class ClusteringValidationEvaluator implements IClassifierEvaluator, ILoggingCustomizable {
 
@@ -61,25 +60,38 @@ public class ClusteringValidationEvaluator implements IClassifierEvaluator, ILog
 			if (!(this.evaluator instanceof IInternalClusteringValidationMeasure)) {
 				throw new IllegalArgumentException("Received data without labels for an external cluster validation measure.");
 			}
-
+/*
 			final ArrayList<Attribute> atts = new ArrayList<>();
 			atts.add(new Attribute("label"));
 			final Instances labels = new Instances("labels", atts, 0);
-			labels.setClassIndex(0);
+			labels.setClassIndex(0);*/
+			final Add add = new Add();
+			add.setAttributeName("label");
+			add.setAttributeType(new SelectedTag("NUM", Add.TAGS_TYPE));
+			add.setAttributeIndex("last");
+			try {
+				add.setInputFormat(this.data);
+			} catch (final Exception e) {
+				e.printStackTrace();
+			}
+
+			Instances labeled = null;
+			try {
+				labeled = Filter.useFilter(this.data, add);
+			} catch (final Exception e) {
+				e.printStackTrace();
+			}
+			labeled.setClassIndex(labeled.numAttributes() - 1);
 
 			try {
 				final double[] results = classifier.classifyInstances(this.data);
 				for (int i = 0; i < this.data.size(); i++) {
-					final double[] values = new double[1];
-					values[0] = results[i];
-					final Instance instance = new DenseInstance(1, values);
-					labels.add(instance);
-					instance.setDataset(labels);
+					labeled.instance(i).setValue(labeled.classIndex(), results[i]);
 				}
 			} catch (final Exception e) {
 				e.printStackTrace();
 			}
-			return this.evaluator.calculateMeasure(labels, this.data);
+			return this.evaluator.calculateMeasure(labeled, null);
 		}
 
 	}

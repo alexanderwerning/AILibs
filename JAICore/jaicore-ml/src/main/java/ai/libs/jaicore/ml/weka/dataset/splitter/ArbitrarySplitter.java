014382c1526bf854b60bbc54247bb144c2b3ed1c
Alexander Werning <awerning@mail.upb.de>
2019-09-08 22:27:01 +0200

    Possibly fixed duplicate arff files

--- JAICore/jaicore-ml/src/main/java/ai/libs/jaicore/ml/weka/dataset/splitter/ArbitrarySplitter.java
+++ JAICore/jaicore-ml/src/main/java/ai/libs/jaicore/ml/weka/dataset/splitter/ArbitrarySplitter.java
@@ -1,9 +1,9 @@
 package ai.libs.jaicore.ml.weka.dataset.splitter;
 
+import ai.libs.jaicore.ml.WekaUtil;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-
-import ai.libs.jaicore.ml.WekaUtil;
 import weka.core.Instances;
 
 /**
@@ -15,6 +15,12 @@ public class ArbitrarySplitter implements IDatasetSplitter {
 
 	@Override
 	public List<Instances> split(final Instances data, final long seed, final double... portions) {
+		// if we copy, scikitlearnwrapper will always create a new file
+		if (portions.length == 1 && portions[0] == 0) {
+			final ArrayList<Instances> ret = new ArrayList<>();
+			ret.add(null);
+			ret.add(data);
+		}
 		return WekaUtil.realizeSplit(data, WekaUtil.getArbitrarySplit(data, new Random(seed), portions));
 	}
 
